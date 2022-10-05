80e034b64df95db651dfdd4882783b3fbc100664
Sandeep singh <sandy180388@gmail.com>
2022-10-05 12:49:20 +0530

    [sandeep] added insert into table file to UC_2

--- Address_Book_ADO/AddressBook.cs
+++ Address_Book_ADO/AddressBook.cs
@@ -11,6 +11,9 @@ namespace Address_Book_ADO
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
         public string PhoneNumber { get; set; }
         public string email { get; set; }
         public string addressBookName { get; set; }
